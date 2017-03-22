f86d323ba72033b26e371d82cef764d7556d8be4
wu.wei <wei.wu@transwarp.io>
2017-03-22 11:21:50 +0800

    Rewrite proxy logic

--- pxy.go
+++ pxy.go
@@ -2,30 +2,70 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
 	"net/http"
-	"net/http/httputil"
+	"strings"
 )
 
+// Pxy is our main struct for proxy releated attributes and methods
 type Pxy struct {
-	proxy *httputil.ReverseProxy
+	// The transport used to send proxy requests to actual server.
+	// If nil, http.DefaultTransport is used.
+	Transport http.RoundTripper
 }
 
+// NewProxy returns a new Pxy object
 func NewProxy() *Pxy {
-	director := func(req *http.Request) {
-		source := req.URL
-
-		req.URL.Scheme = source.Scheme
-		req.URL.Host = source.Host
-	}
-
-	return &Pxy{
-		proxy: &httputil.ReverseProxy{Director: director},
-	}
+	return &Pxy{}
 }
 
-func (p *Pxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Host)
-	p.proxy.ServeHTTP(w, r)
+// ServeHTTP is the main handler for all requests.
+func (p *Pxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Received request %s %s %s\n",
+		req.Method,
+		req.Host,
+		req.RemoteAddr,
+	)
+
+	transport := p.Transport
+	if transport == nil {
+		transport = http.DefaultTransport
+	}
+
+	// copy the origin request, and modify according to proxy
+	// standard and user rules.
+	outReq := new(http.Request)
+	*outReq = *req // this only does shallow copies of maps
+
+	// Set `x-Forwarded-For` header.
+	// `X-Forwarded-For` contains a list of servers delimited by comma and space
+	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+		if prior, ok := outReq.Header["X-Forwarded-For"]; ok {
+			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+		}
+		outReq.Header.Set("X-Forwarded-For", clientIP)
+	}
+
+	// send the modified request and get response
+	res, err := transport.RoundTrip(outReq)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	// write response back to client, including status code, header and body
+
+	for key, value := range res.Header {
+		// Some header item can contains many values
+		for _, v := range value {
+			rw.Header().Add(key, v)
+		}
+	}
+
+	rw.WriteHeader(res.StatusCode)
+	io.Copy(rw, res.Body)
+	res.Body.Close()
 }
 
 func main() {
